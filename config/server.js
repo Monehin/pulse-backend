5deca1366b503c6b360869d824f6d2826a42508b
Monehin Emmanuel <e.monehin@live.com>
2021-01-19 15:45:31 +0200

    update db env

--- config/server.js
+++ config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '637b4289972ae1f576966cc4535f7cb3'),
+      secret: env("ADMIN_JWT_SECRET", "637b4289972ae1f576966cc4535f7cb3"),
     },
   },
 });
