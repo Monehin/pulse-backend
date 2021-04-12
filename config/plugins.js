91a01b4db2f6adfe15412b4e9a8e065286e7f200
monehin <e.monehin@live.com>
2021-04-12 10:05:20 +0200

    added cloudinary for media upload

--- config/plugins.js
+++ config/plugins.js
@@ -1,4 +1,12 @@
-module.exports = {
+module.exports = ({ env }) => ({
+  upload: {
+    provider: "cloudinary",
+    providerOptions: {
+      cloud_name: env("CLOUDINARY_NAME"),
+      api_key: env("CLOUDINARY_KEY"),
+      api_secret: env("CLOUDINARY_SECRET"),
+    },
+  },
   //
   graphql: {
     endpoint: "/graphql",
@@ -10,4 +18,4 @@ module.exports = {
       tracing: true,
     },
   },
-};
+});
