2801a666d5a021d4e9b4951256f64e9f61ebd46d
ceelogre <sam.nishimwe@yahoo.com>
2021-04-27 08:47:47 +0200

    ch: add db configs
    feat: add production build
    
    feat(db) use URI instead of separate configs
    
    feat(db) separate configs based on env
    
    feat(db) add default db
    
    fix: remove access keys

--- config/env/development/database.js
+++ config/env/development/database.js
@@ -4,13 +4,16 @@ module.exports = ({ env }) => ({
     default: {
       connector: "mongoose",
       settings: {
-        host: env("DATABASE_HOST"),
-        port: env.int("DATABASE_PORT"),
+        host: env("DATABASE_HOST", "127.0.0.1"),
+        port: env.int("DATABASE_PORT", 27017),
         database: env("DATABASE_NAME"),
         username: env("DATABASE_USERNAME"),
         password: env("DATABASE_PASSWORD"),
       },
-      options: {},
+      options: {
+        authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
+        ssl: env.bool("DATABASE_SSL", false),
+      },
     },
   },
 });
